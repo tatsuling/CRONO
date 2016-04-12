93f2c5e16d5287c4f51e2f81e9eae35bfd0bd551
Gregory Lee <tatsuling@gmail.com>
2016-04-12 15:29:41 -0600

    Add output of time taken during ROI and total memory usage.

--- libs/roi.cc
+++ libs/roi.cc
@@ -15,8 +15,13 @@ static double time_end;
 #include "ptlcalls.h"
 #endif
 
+#include <sys/resource.h>
+
+#define stringify_value(x) #x
+#define stringify(x) stringify_value(x)
+
 void roi_begin() {
-    std::cout << ROI_PREFIX " Entering ROI" << std::endl;
+    std::cout << stringify(ROI_PREFIX) " Entering ROI" << std::endl;
     std::cout.flush();
 
     #ifdef ENABLE_TIMING
@@ -27,18 +32,28 @@ void roi_begin() {
 
     #ifdef ENABLE_PTLCALLS
     char * snapshot_name = getenv("SNAPSHOT");
-    ptlcall_checkpoint_and_shutdown( snapshot_name );
+    if ( snapshot_name ) {
+        ptlcall_checkpoint_and_shutdown( snapshot_name );
+    }
+    #endif
 
-    std::cout << ROI_PREFIX " Entering SIMULATION" << std::endl;
-    std::cout.flush();
+    #ifdef ENABLE_PTLCALLS
+    char * enter_sim = getenv("SIMULATE");
+    if ( enter_sim && enter_sim[0] == '1' ) {
+        std::cout << stringify(ROI_PREFIX) " Entering SIMULATION" << std::endl;
+        std::cout.flush();
 
-    ptlcall_switch_to_sim();
+        ptlcall_switch_to_sim();
+    }
     #endif
 }
 
 void roi_end() {
     #ifdef ENABLE_PTLCALLS
-    ptlcall_switch_to_native();
+    char * enter_sim = getenv("SIMULATE");
+    if ( enter_sim && enter_sim[0] == '1' ) {
+        ptlcall_switch_to_native();
+    }
     #endif
 
     #if ENABLE_TIMING
@@ -47,7 +62,14 @@ void roi_end() {
     time_end = (double)t.tv_sec+(double)t.tv_usec*1e-6;
     #endif
 
-    std::cout << ROI_PREFIX " Leaving ROI\n" << std::endl;
+    std::cout << stringify(ROI_PREFIX) " Leaving ROI" << std::endl;
     std::cout.flush();
+
+    std::cout << stringify(ROI_PREFIX) " Runtime: " << (time_end - time_begin) << std::endl;
+
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+    
+    std::cout << stringify(ROI_PREFIX) " Memory: " << (double)usage.ru_maxrss/1024 << " MB" << std::endl;
 }
 
