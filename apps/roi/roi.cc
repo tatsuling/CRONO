69d54cf5f1019288f23a5834056a3e645c267c33
Gregory Lee <tatsuling@gmail.com>
2016-04-19 10:36:32 -0600

    Add missing roi.cc

---
+++ apps/roi/roi.cc
@@ -0,0 +1,77 @@
+/*
+ * 
+ * */
+
+#include <iostream>
+#include "roi.h"
+
+#ifdef ENABLE_TIMING
+#include <sys/time.h>
+static double time_begin;
+static double time_end;
+#endif
+
+#ifdef ENABLE_PTLCALLS
+#include "ptlcalls.h"
+#endif
+
+#include <sys/resource.h>
+
+#define stringify_value(x) #x
+#define stringify(x) stringify_value(x)
+
+void roi_begin() {
+    std::cout << stringify(ROI_PREFIX) " Entering ROI" << std::endl;
+    std::cout.flush();
+
+    #ifdef ENABLE_TIMING
+    struct timeval t;
+    gettimeofday(&t,NULL);
+    time_begin = (double)t.tv_sec+(double)t.tv_usec*1e-6;
+    #endif
+
+    #ifdef ENABLE_PTLCALLS
+    char * snapshot_name = getenv("SNAPSHOT");
+    if ( snapshot_name ) {
+        ptlcall_checkpoint_and_shutdown( snapshot_name );
+    }
+    #endif
+
+    #ifdef ENABLE_PTLCALLS
+    char * enter_sim = getenv("SIMULATE");
+    if ( enter_sim && enter_sim[0] == '1' ) {
+        std::cout << stringify(ROI_PREFIX) " Entering SIMULATION" << std::endl;
+        std::cout.flush();
+
+        ptlcall_switch_to_sim();
+    }
+    #endif
+}
+
+void roi_end() {
+    #ifdef ENABLE_PTLCALLS
+    char * enter_sim = getenv("SIMULATE");
+    if ( enter_sim && enter_sim[0] == '1' ) {
+        ptlcall_switch_to_native();
+    }
+    #endif
+
+    #ifdef ENABLE_TIMING
+    struct timeval t;
+    gettimeofday(&t,NULL);
+    time_end = (double)t.tv_sec+(double)t.tv_usec*1e-6;
+    #endif
+
+    std::cout << stringify(ROI_PREFIX) " Leaving ROI" << std::endl;
+    std::cout.flush();
+
+    #ifdef ENABLE_TIMING
+    std::cout << stringify(ROI_PREFIX) " Runtime: " << (time_end - time_begin) << std::endl;
+    #endif
+
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+    
+    std::cout << stringify(ROI_PREFIX) " Memory: " << (double)usage.ru_maxrss/1024 << " MB" << std::endl;
+}
+
